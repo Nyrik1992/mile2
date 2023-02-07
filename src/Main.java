13a6ffa3b58a9f0372a3acbc45e8dc24012c0b39
Nyrik1992 <mirgaleeva92@mail.ru>
2023-02-07 15:05:37 +0600

    Calculate

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 10;
-//        boolean spent = true;
-
-
-        int mile;
-        if (ticketPrice > 20) {
-
-            mile = ticketPrice / 20;
-        } else {
-            mile = 0;
-        }
-
-
-        System.out.println("Ваш бонус: " + mile);
+        BonusMilesService service = new BonusMilesService();
+        int price = 15;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
